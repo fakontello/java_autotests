02250f81bd2ad4ca40b8511aab86ed51f89b6c23
fakontello <fakontello@gmail.com>
2022-03-23 12:47:39 +0300

    new classes

---
+++ src/main/java/ru/gazprombank/Rectangle.java
@@ -0,0 +1,11 @@
+package ru.gazprombank;
+
+public class Rectangle {
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ src/main/java/ru/gazprombank/Square.java
@@ -0,0 +1,8 @@
+package ru.gazprombank;
+
+public class Square {
+    public double l;
+    public Square(double l) {
+        this.l = l;
+    }
+}
