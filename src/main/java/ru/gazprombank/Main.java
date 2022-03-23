a29f2ea64b30e7bb23dd4aee6966c3c815d3b5b8
fakontello <fakontello@gmail.com>
2022-03-23 15:03:30 +0300

    class Point

--- src/main/java/ru/gazprombank/Main.java
+++ src/main/java/ru/gazprombank/Main.java
@@ -10,6 +10,9 @@ public class Main {
 
         Rectangle r = new Rectangle(5, 7);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p = new Point(5, 6, 8, 10);
+        System.out.println("Расстояние между точками = " + p.distance());
     }
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
