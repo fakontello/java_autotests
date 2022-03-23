f6e0741bd5869b0b8494f0abe2e5df5e1e489b68
fakontello <fakontello@gmail.com>
2022-03-23 17:54:49 +0300

    Update Main.java

--- src/main/java/ru/gazprombank/Main.java
+++ src/main/java/ru/gazprombank/Main.java
@@ -18,9 +18,6 @@ public class Main {
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
 
 }
 
