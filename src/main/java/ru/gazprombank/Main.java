2f2621f479456fbc319a9a36866c088d0e3f4a66
fakontello <fakontello@gmail.com>
2022-03-23 12:26:12 +0300

    added several functions

--- src/main/java/ru/gazprombank/Main.java
+++ src/main/java/ru/gazprombank/Main.java
@@ -1,13 +1,24 @@
 package ru.gazprombank;
 
 public class Main {
-    public static void main(String[] args) {
-        String some = "world";
-        System.out.println("Hello, " + some + "!");
 
-        double l = 6;
-        double w = l * l + l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + w);
+    public static void main(String[] args) {
+        hello("world");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 5;
+        double b = 7;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double len) {
+        return len * len;
+    }
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
 
