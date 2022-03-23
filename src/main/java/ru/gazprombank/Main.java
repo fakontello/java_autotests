a4ffb0be790345b44fc4dc4312b9de69f87b94c4
fakontello <fakontello@gmail.com>
2022-03-23 11:17:30 +0300

    Update Main.java

--- src/main/java/ru/gazprombank/Main.java
+++ src/main/java/ru/gazprombank/Main.java
@@ -2,7 +2,12 @@ package ru.gazprombank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String some = "world";
+        System.out.println("Hello, " + some + "!");
+
+        double l = 6;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
 
